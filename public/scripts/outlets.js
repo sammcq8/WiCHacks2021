69cb04ab3e8b2b6162051e9a1a2e231a368f9d24
jessi <jcm9472@rit.edu>
2021-02-28 11:32:09 -0500

    outlets

--- public/scripts/outlets.js
+++ public/scripts/outlets.js
@@ -1,54 +1,43 @@
 const outletList = document.querySelector('#outlets-list')
-var totalkWh = 0;
 
-function renderOutletList(doc) {
+function renderOutletList(doc, i) {
     let li = document.createElement('li');
     let name = document.createElement('span');
     let kWh = document.createElement('span');
+    let category = document.createElement('span');
 
     li.setAttribute('data-id', doc.id);
     name.textContent = doc.data().name;
-    kWh.textContent = "Power Usage: "+ doc.data().kWh + "kWh";
+    kWh.textContent = "Power Used this Month: "+ doc.data().kWh + "kWh";
+    category.textContent = "Category: " + doc.data().category;
+
+	console.log(category);
+
+    name.setAttribute("id", "device" + i);
 
     li.appendChild(name);
     li.appendChild(kWh);
+    li.appendChild(category);
 
-    totalkWh = kWh + totalkWh;
+
     outletList.appendChild(li);
+    return doc.data().kWh;
 }
 
-function addTotal(kwh){
-    totalkWh+=kwh;
-}
+
 
 // getting data
 db.collection('electronicDevices').get().then(snapshot => {
-    
-    snapshot.docs.forEach(doc => {
-        
-        //if (doc.data().user == db.collection("users").doc("CurrentlyLoggedIn").data().name){
-            renderOutletList(doc);
-        //}
-        
-    });
+    let totalkWh = 0;
+    for(i = 0; i < snapshot.docs.length; i++){
+        let kWh = renderOutletList(snapshot.docs[i], i);
+	totalkWh += kWh;
+
+	if (snapshot.docs[i].data().status == "on"){
+		document.getElementById("device" + i).style.color = "green";
+	}
+    }
+    document.getElementById("totalEnergy").innerHTML += totalkWh += " kWh";
 });
 
-db.collection('electronicDevices').get().then(snapshot => {
-    snapshot.docs.forEach(doc => {
-        addTotal(doc.data().kWh)
-    });
-})
 
-
-let li = document.createElement('li');
-let title = document.createElement('span');
-let kWh = document.createElement('span');
-
-li.setAttribute('data-id', doc.id);
-title.textContent = "All Devices:";
-kWh.textContent = "Total Power Usage: " + totalkWh + "kWh";
-
-li.appendChild(title);
-li.appendChild(kWh);
-
-outletList.appendChild(li);
